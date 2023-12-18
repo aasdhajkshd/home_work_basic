8d54f9af9acf6eede84abfa0a95b99c8f65eb429
Vasiliy K <vasiliy.kotikov@auxo.team>
2023-12-18 22:26:19 +0300

    HW03 Chessboard

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,3 +1,6 @@
+// Шахматная доска
+// Программа, которая создаёт строку, содержащую решётку 8х8, а линии разделяются символами новой строки.
+
 package main
 
 import (
@@ -5,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func yesno(question string) bool {
@@ -19,11 +23,11 @@ func yesno(question string) bool {
 
 func size() int {
 	var answer int
-	for i := 0; i < 3; i++ {
-		_, e := fmt.Fscanln(os.Stdin, &answer)
+	for i := 0; i < 3; i++ { // три попытки на неверный ввод, так как ожидается цифра
+		_, e := fmt.Fscanln(os.Stdin, &answer) // сюда еще добавить ограничение по цифре по range
 		if e != nil {
 			fmt.Println("Ошибка:", e)
-			if yesno("Попробовать еще раз:") {
+			if yesno("Попробовать еще раз:") { // здесь можно ответить и нет, тогда уходим в else
 				continue
 			} else {
 				fmt.Println("... на нет и спроса нет, тогда 8")
@@ -37,16 +41,17 @@ func size() int {
 
 func draw(x, y int) {
 	hash, pipe := "#", "|"
-	for i := 0; i < y; i++ {
-		for j := 0; j < x; j++ {
+	for i := 0; i < y; i++ { // для прохода по строкам (вертикаль)
+		for j := 0; j < x; j++ { // для прохода по столбцам (горизонталь)
+			time.Sleep(700 * time.Millisecond) // ... как на матричном принтере
 			fmt.Printf("%s", pipe)
-			if j%2 == i%2 {
+			if j%2 == i%2 { // вот здесь меняем местами "шашечки" по строкам, чтобы были вдоль
 				fmt.Printf("%s", hash)
 			} else {
 				fmt.Printf(" ")
 			}
 		}
-		fmt.Println(pipe)
+		fmt.Println(pipe) // ставится завершающая линия и перенос строки
 	}
 }
 
