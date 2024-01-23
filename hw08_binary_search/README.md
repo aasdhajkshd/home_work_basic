6dd2b3ad201fe7db75ec3dadc60a82af0e595616
Vasiliy K <vasiliy.kotikov@auxo.team>
2024-01-23 13:39:22 +0300

    hw08_binary_search#1

--- hw08_binary_search/README.md
+++ hw08_binary_search/README.md
@@ -8,15 +8,3 @@
 - Понятность и чистота кода - до 2 баллов
 
 #### Зачёт от 4 баллов
-
-#### Список
-
-- [In-depth introduction to bufio.Scanner in Golang](https://medium.com/golangspec/in-depth-introduction-to-bufio-scanner-in-golang-55483bb689b4)
-- [kgrz/reading-files-in-go](https://github.com/kgrz/reading-files-in-go/blob/master/comma-separated-string.go)
-- [50 оттенков Go: ловушки, подводные камни и распространённые ошибки новичков](https://habr.com/ru/companies/vk/articles/314804/)
-- [How to Check If a Map Key Exists in Go](https://freshman.tech/snippets/go/check-map-key/)
-- [Package bytes](https://pkg.go.dev/bytes@go1.21.6)
-- [func ScanLines](https://cs.opensource.google/go/go/+/refs/tags/go1.21.6:src/bufio/scan.go;l=350)
-- [Разбираемся в Go: пакеты bytes и strings](https://habr.com/ru/articles/307554/)
-- [Finding the index value of specified byte in slice of bytes in Golang](https://www.geeksforgeeks.org/finding-the-index-value-of-specified-byte-in-slice-of-bytes-in-golang/)
-- [func Equal](https://pkg.go.dev/bytes#example-Equal)
--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,83 +1,81 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/base64"
 	"fmt"
-	"os"
-	"strings"
+	"math/rand"
+	"slices"
+	_ "sort"
 )
 
-func trimSpaceCR(data []byte) []byte {
-	if len(data) > 0 && data[len(data)-1] == '\r' {
-		return bytes.TrimSpace(data[0 : len(data)-1])
-	}
-	return bytes.TrimSpace(data)
+type rangeRand struct {
+	min, max, num int
+	nums          []int
 }
 
-func scanComma(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	sepIndex := bytes.IndexByte(data, ',')
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
+//nolint:gosec
+func (r *rangeRand) Random() ([]int, error) {
+	if r.max <= 0 || r.max-1 == r.min {
+		return nil, fmt.Errorf("здесь 100%% результат") // плохой диапазон будет, просто отдадим ошибку
 	}
-	if sepIndex > 0 {
-		return sepIndex + 1, trimSpaceCR(data[:sepIndex]), nil
-	}
-	if i := bytes.IndexByte(data, '\n'); i >= 0 {
-		return i + 1, trimSpaceCR(data[0:i]), nil
-	}
-	if atEOF {
-		return len(data), trimSpaceCR(data), nil
-	}
-	return 0, trimSpaceCR(data), bufio.ErrFinalToken
-}
-
-func readFile(filePath string) ([][]byte, error) {
-	fileData, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("error: %w", err)
-	}
-	d := make([]byte, base64.StdEncoding.DecodedLen(len(fileData)))
-	n, err := base64.StdEncoding.Decode(d, fileData)
-	if err != nil {
-		return nil, fmt.Errorf("decode error: %w", err)
-	}
-	wordsList := [][]byte{}
-	s := bufio.NewScanner(bytes.NewReader(d[:n]))
-	s.Buffer(d, bufio.MaxScanTokenSize)
-	s.Split(scanComma)
-	for s.Scan() {
-		wordsList = append(wordsList, s.Bytes())
-	}
-	// fmt.Printf("%s", wordsList)
-	err = s.Err()
-	return wordsList, err
-}
-
-func findMatch(wordStr string, sliceBytes [][]byte) bool {
-	for _, j := range sliceBytes {
-		if bytes.Equal([]byte(strings.ToLower(wordStr)), bytes.ToLower(j)) {
-			return true
+	nums := make(map[int]bool, r.num)
+	for i := 0; i < r.num; i++ {
+		v := r.min + rand.Intn(r.max-r.min+1)
+		if _, found := nums[v]; found {
+			nums[v] = true // для понимания количества уникальных чисел
+		} else {
+			nums[v] = false
+			// реализовывал и на crypto, для домашнего, без велосипеда, тут логига другая
+			r.nums = append(r.nums, v)
 		}
 	}
-	return false
+	// fmt.Printf("Random r.nums: %+v\n", r.nums)
+	return r.nums, nil
+}
+
+// на вход принимает слайс чисел и число для поиска.
+func binarySearch(r []int, i int) int {
+	k := make([]int, len(r))
+	copy(k, r) // да, нужно клонировать или новый append slice
+	slices.Sort(r)
+	midValue := len(r) / 2
+	lowValue := slices.Min(r)
+	highValue := slices.Max(r)
+	// fmt.Println("binarySearch:", lowValue, midValue, highValue, r)
+	for _, j := range r {
+		if i > j {
+			lowValue = midValue + 1
+		} else {
+			highValue = midValue - 1
+		}
+		midValue = (lowValue + highValue) // 2
+		if j == i {
+			fmt.Println("! по классике после сортировки нашли - ", i, "из", r)
+		}
+	}
+	f := slices.Index(k, i)
+	fmt.Println("! в пакете slices без сортировки позиция", f+1, "из", k)
+	return f // возвращает этот метод позиция найденного числа
 }
 
 func main() {
-	byteWords, _ := readFile("list_test.enc")
-	var strInput string
-	fmt.Printf("Введите название города России, попробуем найти его в нашем списке... \n> ")
-	s := bufio.NewScanner(os.Stdin)
-	if s.Scan() {
-		strInput = s.Text()
+	var r rangeRand
+	r.min = 9
+	r.max = 29
+	r.num = 6
+	var e []int
+	if v, err := r.Random(); err != nil {
+		e = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	} else {
+		e = v
 	}
-	if len(strInput) > 0 {
-		fmt.Printf("Ваше слово: %s", strInput)
-		if findMatch(strInput, byteWords) {
-			fmt.Println("... eсть совпадение")
-		} else {
-			fmt.Println("... не найдено")
-		}
+	fmt.Println("поисковая комбинация:", e)
+	// fmt.Println("Main:", r)
+	var i int
+	fmt.Printf("Прекрасная игра в угадай цифру, укажите её от %d до %d\n> ", r.min, r.max)
+	fmt.Scanln(&i)
+	if i >= r.min && i <= r.max {
+		fmt.Println("вот что мы нашли...:", binarySearch(e, i)) // или -1, если числа нет.
+	} else {
+		fmt.Println("указано значение вне диапазона")
 	}
 }
