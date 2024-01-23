7074e70000a59acc6f4076aeb0562e759d327672
Vasiliy K <vasiliy.kotikov@auxo.team>
2024-01-23 22:27:22 +0300

    hw08_binary_search#2

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -2,80 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"slices"
-	_ "sort"
 )
 
-type rangeRand struct {
-	min, max, num int
-	nums          []int
+type binaryStruct struct {
+	lowValue, midValue, highValue int
 }
 
-//nolint:gosec
-func (r *rangeRand) Random() ([]int, error) {
-	if r.max <= 0 || r.max-1 == r.min {
-		return nil, fmt.Errorf("здесь 100%% результат") // плохой диапазон будет, просто отдадим ошибку
-	}
-	nums := make(map[int]bool, r.num)
-	for i := 0; i < r.num; i++ {
-		v := r.min + rand.Intn(r.max-r.min+1)
-		if _, found := nums[v]; found {
-			nums[v] = true // для понимания количества уникальных чисел
+func (b *binaryStruct) binarySearch(inputSlice []int, searchValue int) int {
+	b.lowValue = 0
+	b.highValue = len(inputSlice) - 1 // len возвращает количество
+	for i, j := range inputSlice {
+		if searchValue > j {
+			b.lowValue = b.midValue + 1
 		} else {
-			nums[v] = false
-			// реализовывал и на crypto, для домашнего, без велосипеда, тут логига другая
-			r.nums = append(r.nums, v)
+			b.highValue = b.midValue - 1
+		}
+		b.midValue = (b.lowValue + b.highValue) / 2
+		if j == searchValue {
+			return i
 		}
 	}
-	// fmt.Printf("Random r.nums: %+v\n", r.nums)
-	return r.nums, nil
-}
-
-// на вход принимает слайс чисел и число для поиска.
-func binarySearch(r []int, i int) int {
-	k := make([]int, len(r))
-	copy(k, r) // да, нужно клонировать или новый append slice
-	slices.Sort(r)
-	midValue := len(r) / 2
-	lowValue := slices.Min(r)
-	highValue := slices.Max(r)
-	// fmt.Println("binarySearch:", lowValue, midValue, highValue, r)
-	for _, j := range r {
-		if i > j {
-			lowValue = midValue + 1
-		} else {
-			highValue = midValue - 1
-		}
-		midValue = (lowValue + highValue) // 2
-		if j == i {
-			fmt.Println("! по классике после сортировки нашли - ", i, "из", r)
-		}
-	}
-	f := slices.Index(k, i)
-	fmt.Println("! в пакете slices без сортировки позиция", f+1, "из", k)
-	return f // возвращает этот метод позиция найденного числа
+	return -1
 }
 
 func main() {
-	var r rangeRand
-	r.min = 9
-	r.max = 29
-	r.num = 6
-	var e []int
-	if v, err := r.Random(); err != nil {
-		e = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	} else {
-		e = v
-	}
-	fmt.Println("поисковая комбинация:", e)
-	// fmt.Println("Main:", r)
-	var i int
-	fmt.Printf("Прекрасная игра в угадай цифру, укажите её от %d до %d\n> ", r.min, r.max)
-	fmt.Scanln(&i)
-	if i >= r.min && i <= r.max {
-		fmt.Println("вот что мы нашли...:", binarySearch(e, i)) // или -1, если числа нет.
-	} else {
-		fmt.Println("указано значение вне диапазона")
-	}
+	bs := binaryStruct{}
+	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	searchValue := 5
+	fmt.Println(bs.binarySearch(inputSlice, searchValue)) // -1, если числа нет.
 }
--- hw08_binary_search/main_test.go
+++ hw08_binary_search/main_test.go
@@ -7,9 +7,10 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
+	bs := binaryStruct{}
 	testSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	testValue := 5      // поставить 11 для получения ошибки
 	expectedResult := 4 // -1 в слайсе
-	result := binarySearch(testSlice, testValue)
+	result := bs.binarySearch(testSlice, testValue)
 	assert.Equal(t, expectedResult, result, "Возникла ошибка проверки функции двоичного поиска")
 }
