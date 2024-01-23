bd24370eb9764203cac9a25a1b2d867a306be8ce
Vasiliy K <vasiliy.kotikov@auxo.team>
2024-01-24 00:29:47 +0300

    hw08_binary_search#3

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -5,21 +5,21 @@ import (
 )
 
 type binaryStruct struct {
-	lowValue, midValue, highValue int
+	lowValue, highValue int
 }
 
 func (b *binaryStruct) binarySearch(inputSlice []int, searchValue int) int {
 	b.lowValue = 0
-	b.highValue = len(inputSlice) - 1 // len возвращает количество
-	for i, j := range inputSlice {
-		if searchValue > j {
-			b.lowValue = b.midValue + 1
-		} else {
-			b.highValue = b.midValue - 1
+	b.highValue = len(inputSlice)
+	for i := b.lowValue; i < b.highValue; i++ {
+		j := (b.lowValue + b.highValue) / 2
+		if inputSlice[j] == searchValue {
+			return j
 		}
-		b.midValue = (b.lowValue + b.highValue) / 2
-		if j == searchValue {
-			return i
+		if searchValue > inputSlice[j] {
+			b.lowValue = j + 1
+		} else {
+			b.highValue = j - 1
 		}
 	}
 	return -1
@@ -27,7 +27,7 @@ func (b *binaryStruct) binarySearch(inputSlice []int, searchValue int) int {
 
 func main() {
 	bs := binaryStruct{}
-	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	searchValue := 5
+	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	searchValue := 11
 	fmt.Println(bs.binarySearch(inputSlice, searchValue)) // -1, если числа нет.
 }
