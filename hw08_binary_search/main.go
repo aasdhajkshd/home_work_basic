da1732e702fe9e91374f68ca5209393fee29c089
Vasiliy K <vasiliy.kotikov@auxo.team>
2024-01-24 14:21:31 +0300

    hw08_binary_search#7

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"os"
 	"slices"
 )
 
@@ -71,8 +70,8 @@ func main() {
 	var inputSlice []int
 	var searchValue int
 
-	fmt.Println(bs.binarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5))
-	os.Exit(0)
+	// fmt.Println(bs.binarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5))
+	// os.Exit(0)
 
 	if v, err := r.Random(); err != nil {
 		inputSlice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
