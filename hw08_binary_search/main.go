ad1454c9c512c7eb9a7fd5659bcd173f2d076036
Vasiliy K <vasiliy.kotikov@auxo.team>
2024-01-24 14:05:57 +0300

    hw08_binary_search#4

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	_ "os"
+	"slices"
 )
 
-type binaryStruct struct {
+type Range struct {
+	min, max, num int
+	nums          []int
+}
+
+type BinaryStruct struct {
 	lowValue, highValue int
 }
 
-func (b *binaryStruct) binarySearch(inputSlice []int, searchValue int) int {
-	b.lowValue = 0
-	b.highValue = len(inputSlice)
-	for i := b.lowValue; i < b.highValue; i++ {
+//nolint:gosec
+func (r *Range) Random() ([]int, error) {
+	if r.max <= 0 || r.max-1 == r.min {
+		return nil, fmt.Errorf("здесь 100%% результат") // плохой диапазон будет, просто отдадим ошибку
+	}
+	nums := make(map[int]bool, r.num)
+	for i := 0; i < r.num; i++ {
+		v := r.min + rand.Intn(r.max-r.min+1)
+		if _, found := nums[v]; found {
+			nums[v] = true // для понимания количества уникальных чисел
+		} else {
+			nums[v] = false
+			// реализовывал и на crypto, для домашнего, без велосипеда, тут логига другая
+			r.nums = append(r.nums, v)
+		}
+	}
+	// fmt.Printf("Random r.nums: %+v\n", r.nums)
+	return r.nums, nil
+}
+
+// на вход принимает слайс чисел и число для поиска.
+func (b *BinaryStruct) binarySearch(inputSlice []int, searchValue int) int {
+	lenSlice := len(inputSlice)
+	if lenSlice == 0 { // нулевой массив
+		return -1
+	}
+	b.lowValue, b.highValue = 0, lenSlice
+	for b.lowValue <= b.highValue { // цикл-условие аналог "while"
+		// fmt.Println("начало цикла:", j, b.lowValue, b.highValue, inputSlice[j], searchValue)
 		j := (b.lowValue + b.highValue) / 2
+		if j >= b.highValue { // запрашиваемое значение вне допустимого диапазона
+			break
+		}
 		if inputSlice[j] == searchValue {
 			return j
 		}
@@ -21,13 +57,42 @@ func (b *binaryStruct) binarySearch(inputSlice []int, searchValue int) int {
 		} else {
 			b.highValue = j - 1
 		}
+		// fmt.Println("завершение цикла:", j, b.lowValue, b.highValue, inputSlice[j], searchValue)
 	}
 	return -1
 }
 
 func main() {
-	bs := binaryStruct{}
-	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	searchValue := 11
-	fmt.Println(bs.binarySearch(inputSlice, searchValue)) // -1, если числа нет.
+	var r Range
+	r.min = 0
+	r.max = 100
+	r.num = 40
+	bs := BinaryStruct{}
+	var inputSlice []int
+	var searchValue int
+
+	// fmt.Println(bs.binarySearch([]int{4, 89, 94, 97, 98}, 99)) 
+	// os.Exit(0)
+
+	if v, err := r.Random(); err != nil {
+		inputSlice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		searchValue = 11
+	} else {
+		inputSlice = v
+		slices.Sort(inputSlice)
+	}
+	fmt.Println("Поисковая комбинация:", inputSlice)
+	// fmt.Println("Main:", r)
+	fmt.Printf("Укажите цифру для поиска в диапазоне от %d до %d\n> ", r.min, r.max)
+	fmt.Scanln(&searchValue)
+	if searchValue >= r.min && searchValue <= r.max {
+		indexSlice := bs.binarySearch(inputSlice, searchValue)
+		if indexSlice < 0 {
+			fmt.Printf("Значение не найдено: %d\n", searchValue)
+		} else {
+			fmt.Printf("Индекс: %d, значение: %d\n", indexSlice, inputSlice[indexSlice])
+		}
+	} else {
+		fmt.Println("указано значение вне допустимого диапазона")
+	}
 }
